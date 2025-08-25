7a73d91578dc9443cf3e3606f0748f9aa034b23c
Orest Yakymiv <akimivorest@gmail.com>
2025-08-25 19:07:02 +0300

    kubernetes manifest and helm charts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-kubectl apply -f .infrastructure/mysql/ns.yml
-kubectl apply -f .infrastructure/mysql/configMap.yml
-kubectl apply -f .infrastructure/mysql/secret.yml
-kubectl apply -f .infrastructure/mysql/service.yml
-kubectl apply -f .infrastructure/mysql/statefulSet.yml
+kubectl apply -f k8s/mysql/ns.yml
+kubectl apply -f k8s/mysql/secret.yml
+kubectl apply -f k8s/mysql/service.yml
+kubectl apply -f k8s/mysql/statefulSet.yml
 
-kubectl apply -f .infrastructure/app/ns.yml
-kubectl apply -f .infrastructure/app/pv.yml
-kubectl apply -f .infrastructure/app/pvc.yml
-kubectl apply -f .infrastructure/app/secret.yml
-kubectl apply -f .infrastructure/app/configMap.yml
-kubectl apply -f .infrastructure/app/clusterIp.yml
-kubectl apply -f .infrastructure/app/nodeport.yml
-kubectl apply -f .infrastructure/app/hpa.yml
-kubectl apply -f .infrastructure/app/deployment.yml
-
-# Install Ingress Controller
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-# kubectl apply -f .infrastructure/ingress/ingress.yml
+kubectl apply -f k8s/app/ns.yml
+kubectl apply -f k8s/app/pv.yml
+kubectl apply -f k8s/app/pvc.yml
+kubectl apply -f k8s/app/secret.yml
+kubectl apply -f k8s/app/configMap.yml
+kubectl apply -f k8s/app/clusterIp.yml
+kubectl apply -f k8s/app/hpa.yml
+kubectl apply -f k8s/app/deployment.yml
+kubectl apply -f k8s/app/ingress.yml
+kubectl apply -f k8s/app/pdb.yml
